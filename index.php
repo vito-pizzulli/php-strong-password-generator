d8441326ff7bc003e42a57d4970aeaabfa662c04
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-04 14:05:12 +0200

    added form in html

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
 </head>
 <body>
+    <h1>Strong Password Generator</h1>
+    <h2>Genera una password sicura</h2>
     
+    <form action="./index.php" method="get">
+        <label for="passwordLength">Lunghezza password:</label>
+        <input type="text" name="passwordLength" id="passwordLength">
+        <button type="submit">Invia</button>
+    </form>
 </body>
 </html>
